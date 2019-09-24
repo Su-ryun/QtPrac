2bc7954a30554ae0b7e3bb4e3970d5291d771824
Su-ryun <dlee67@msudenver.edu>
2019-09-24 00:01:17 -0600

    Now, getting ready to deploy an example app.

--- ch02-HelloFromQt/mainwindow.cpp
+++ ch02-HelloFromQt/mainwindow.cpp
@@ -12,3 +12,10 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    // Sometimes, Qt creator won't recognize that certain objects are legitament, unless I build the project.
+    QString name = ui->lineEdit->selectedText();
+        ui->label->setText(QString("Nice to meet you %1!").arg(name));
+}
